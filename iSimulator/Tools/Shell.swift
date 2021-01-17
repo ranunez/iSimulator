3c8033fb395e7ae8cc2563c9c9c3b6d50ad32c9d
Ricardo Nunez <rnunez@mdsol.com>
2021-01-17 15:35:45 -0800

    Correctly handles xcrun errors

--- iSimulator/Tools/Shell.swift
+++ iSimulator/Tools/Shell.swift
@@ -8,15 +8,34 @@
 
 import Foundation
 
+struct XCRunError: Error {
+    let command: String
+    let message: String
+}
+
 @discardableResult
-func xcrun(arguments: String...) -> Data {
+func xcrun(arguments: String...) -> Result<Data, XCRunError> {
+    let launchPath = "/usr/bin/xcrun"
     let process = Process()
-    process.launchPath = "/usr/bin/xcrun"
+    process.launchPath = launchPath
     process.arguments = arguments
+    
     let outputPipe = Pipe()
     process.standardOutput = outputPipe
+    
+    let errorPipe = Pipe()
+    process.standardError = errorPipe
+    
     process.launch()
     
     let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-    return outputData
+    if outputData.isEmpty {
+        let command = "\(launchPath) \(arguments.reduce("", { "\($0) \($1)" }))"
+        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+        let errorMessage = String(data: errorData, encoding: .utf8) ?? ""
+        let error = XCRunError(command: command, message: errorMessage)
+        return .failure(error)
+    } else {
+        return .success(outputData)
+    }
 }
--- iSimulator/UI/MainMenu.swift
+++ iSimulator/UI/MainMenu.swift
@@ -42,124 +42,131 @@ final class MainMenu: NSMenu {
             self.watchQueue?.removeAllPaths()
             self.watchQueue?.addPath(Device.url.path)
             
-            let xcodePathData = xcrun(arguments: "xcode-select", "-p")
-            
-            let shouldUpdateCache: Bool
-            if let xcodePath = String(data: xcodePathData, encoding: .utf8), self.lastXcodePath != xcodePath {
-                self.lastXcodePath = xcodePath
-                shouldUpdateCache = true
-            } else if isForceUpdate {
-                shouldUpdateCache = true
-            } else {
-                shouldUpdateCache = false
-            }
-            
-            if shouldUpdateCache {
-                self.appCache = ApplicationCache()
-                DispatchQueue.main.async {
-                    let rootLinkURL = UserDefaults.standard.rootLinkURL
-                    let contents = try? FileManager.default.contentsOfDirectory(at: rootLinkURL,
-                                                                                includingPropertiesForKeys: [.isHiddenKey],
-                                                                                options: [.skipsPackageDescendants, .skipsSubdirectoryDescendants])
-                    contents?.filter({ $0.pathComponents.last == "Icon\r" }).forEach({ _ in try? FileManager.default.removeItem(at: rootLinkURL) })
-                    try? FileManager.default.createDirectory(at: rootLinkURL, withIntermediateDirectories: true)
-                    NSWorkspace.shared.setIcon(#imageLiteral(resourceName: "statusItem_icon"), forFile: rootLinkURL.path, options:[])
-                }
-            }
-            
-            let jsonData = xcrun(arguments: "simctl", "list", "-j")
-            guard let json = try? JSONSerialization.jsonObject(with: jsonData, options: .allowFragments) as? [String: Any] else {
-                fatalError()
-            }
-            
-            if let rawRunTimes = json["runtimes"] as? [[String: Any]], let rawDevices = json["devices"] as? [String: [[String: Any]]] {
-                self.runtimes = rawRunTimes.map({ iSimulator.Runtime(json: $0) })
-                
-                let devices = rawDevices.reduce([String: [Device]]()) { (result, rawDevice) -> [String: [Device]] in
-                    var updatedResult = result
-                    updatedResult[rawDevice.key] = rawDevice.value.map({ iSimulator.Device(json: $0) })
-                    return updatedResult
-                }
-                
-                var urlAndAppDicCache = [URL: Application]()
-                var sandboxURLsCache = Set<URL>()
-                self.runtimes.forEach { runtime in
-                    runtime.devices = devices[runtime.name] ?? devices[runtime.identifier] ?? []
-                    
-                    runtime.devices.forEach {
-                        $0.runtime = runtime
-                        $0.updateApps(with: self.appCache)
-                        $0.updateAppGroups()
-                        
-                        $0.applications.forEach { app in
-                            app.removeLinkDir()
-                            urlAndAppDicCache[app.bundleDirUrl] = app
-                            sandboxURLsCache.insert(app.sandboxDirUrl)
-                        }
-                    }
-                }
-                
-                self.appCache.urlAndAppDic = urlAndAppDicCache
-                self.appCache.sandboxURLs = sandboxURLsCache
-                
-                self.runtimes.forEach { runtime in
-                    runtime.devices.forEach { device in
-                        self.watchQueue?.addPath(device.dataURL.path)
-                        if FileManager.default.fileExists(atPath: device.bundleURL.path) {
-                            self.watchQueue?.addPath(device.bundleURL.path)
-                        }
-                    }
-                }
-                
-                DispatchQueue.main.async {
-                    let deviceInfoURLPaths = self.runtimes.flatMap({ $0.devices }).compactMap({ $0.infoURL.path })
-                    _ = try? FileWatch(paths: deviceInfoURLPaths, eventHandler: { [weak self] eventFlag in
-                        if eventFlag.contains(.ItemIsFile) && eventFlag.contains(.ItemRenamed) {
-                            self?.refresh(isForceUpdate: false)
-                        }
-                    })
-                }
-            } else {
-                self.runtimes = []
-                self.appCache.urlAndAppDic = [:]
-                self.appCache.sandboxURLs = []
-            }
-            
-            var deviceItems = self.runtimes.sorted(by: { $0.name < $1.name }).flatMap { runtime -> [NSMenuItem] in
-                let hasAppDeviceItems: [NSMenuItem] = runtime.devices.filter({ !$0.applications.isEmpty }).compactMap { DeviceMenuItem($0) }
-                if hasAppDeviceItems.isEmpty {
-                    return []
+            switch xcrun(arguments: "xcode-select", "-p") {
+            case .success(let xcodePathData):
+                let shouldUpdateCache: Bool
+                if let xcodePath = String(data: xcodePathData, encoding: .utf8), self.lastXcodePath != xcodePath {
+                    self.lastXcodePath = xcodePath
+                    shouldUpdateCache = true
+                } else if isForceUpdate {
+                    shouldUpdateCache = true
                 } else {
-                    let titleItem = NSMenuItem(title: runtime.name,
-                                               action: nil,
-                                               keyEquivalent: "")
-                    titleItem.isEnabled = false
-                    
-                    var items = [NSMenuItem]()
-                    items.append(titleItem)
-                    items.append(contentsOf: hasAppDeviceItems)
-                    return items
+                    shouldUpdateCache = false
                 }
-            }
-            
-            if self.runtimes.contains(where: { $0.devices.contains(where: { $0.applications.isEmpty }) }) {
-                deviceItems.append(NSMenuItem.separator())
-            }
-            
-            if deviceItems.isEmpty {
-                let xcodeSelectItem = NSMenuItem(title: "Xcode Select...",
-                                                 action: #selector(self.openPreferences),
-                                                 keyEquivalent: "")
-                xcodeSelectItem.target = self
-                deviceItems.append(xcodeSelectItem)
-            }
-            
-            deviceItems.append(NSMenuItem.separator())
-            deviceItems.append(contentsOf: self.commonItems)
-            
-            DispatchQueue.main.async {
-                self.removeAllItems()
-                deviceItems.forEach { self.addItem($0) }
+                
+                if shouldUpdateCache {
+                    self.appCache = ApplicationCache()
+                    DispatchQueue.main.async {
+                        let rootLinkURL = UserDefaults.standard.rootLinkURL
+                        let contents = try? FileManager.default.contentsOfDirectory(at: rootLinkURL,
+                                                                                    includingPropertiesForKeys: [.isHiddenKey],
+                                                                                    options: [.skipsPackageDescendants, .skipsSubdirectoryDescendants])
+                        contents?.filter({ $0.pathComponents.last == "Icon\r" }).forEach({ _ in try? FileManager.default.removeItem(at: rootLinkURL) })
+                        try? FileManager.default.createDirectory(at: rootLinkURL, withIntermediateDirectories: true)
+                        NSWorkspace.shared.setIcon(#imageLiteral(resourceName: "statusItem_icon"), forFile: rootLinkURL.path, options:[])
+                    }
+                }
+                
+                switch xcrun(arguments: "simctl", "list", "-j") {
+                case .success(let jsonData):
+                    guard let json = try? JSONSerialization.jsonObject(with: jsonData, options: .allowFragments) as? [String: Any] else {
+                        fatalError()
+                    }
+                    
+                    if let rawRunTimes = json["runtimes"] as? [[String: Any]], let rawDevices = json["devices"] as? [String: [[String: Any]]] {
+                        self.runtimes = rawRunTimes.map({ iSimulator.Runtime(json: $0) })
+                        
+                        let devices = rawDevices.reduce([String: [Device]]()) { (result, rawDevice) -> [String: [Device]] in
+                            var updatedResult = result
+                            updatedResult[rawDevice.key] = rawDevice.value.map({ iSimulator.Device(json: $0) })
+                            return updatedResult
+                        }
+                        
+                        var urlAndAppDicCache = [URL: Application]()
+                        var sandboxURLsCache = Set<URL>()
+                        self.runtimes.forEach { runtime in
+                            runtime.devices = devices[runtime.name] ?? devices[runtime.identifier] ?? []
+                            
+                            runtime.devices.forEach {
+                                $0.runtime = runtime
+                                $0.updateApps(with: self.appCache)
+                                $0.updateAppGroups()
+                                
+                                $0.applications.forEach { app in
+                                    app.removeLinkDir()
+                                    urlAndAppDicCache[app.bundleDirUrl] = app
+                                    sandboxURLsCache.insert(app.sandboxDirUrl)
+                                }
+                            }
+                        }
+                        
+                        self.appCache.urlAndAppDic = urlAndAppDicCache
+                        self.appCache.sandboxURLs = sandboxURLsCache
+                        
+                        self.runtimes.forEach { runtime in
+                            runtime.devices.forEach { device in
+                                self.watchQueue?.addPath(device.dataURL.path)
+                                if FileManager.default.fileExists(atPath: device.bundleURL.path) {
+                                    self.watchQueue?.addPath(device.bundleURL.path)
+                                }
+                            }
+                        }
+                        
+                        DispatchQueue.main.async {
+                            let deviceInfoURLPaths = self.runtimes.flatMap({ $0.devices }).compactMap({ $0.infoURL.path })
+                            _ = try? FileWatch(paths: deviceInfoURLPaths, eventHandler: { [weak self] eventFlag in
+                                if eventFlag.contains(.ItemIsFile) && eventFlag.contains(.ItemRenamed) {
+                                    self?.refresh(isForceUpdate: false)
+                                }
+                            })
+                        }
+                    } else {
+                        self.runtimes = []
+                        self.appCache.urlAndAppDic = [:]
+                        self.appCache.sandboxURLs = []
+                    }
+                    
+                    var deviceItems = self.runtimes.sorted(by: { $0.name < $1.name }).flatMap { runtime -> [NSMenuItem] in
+                        let hasAppDeviceItems: [NSMenuItem] = runtime.devices.filter({ !$0.applications.isEmpty }).compactMap { DeviceMenuItem($0) }
+                        if hasAppDeviceItems.isEmpty {
+                            return []
+                        } else {
+                            let titleItem = NSMenuItem(title: runtime.name,
+                                                       action: nil,
+                                                       keyEquivalent: "")
+                            titleItem.isEnabled = false
+                            
+                            var items = [NSMenuItem]()
+                            items.append(titleItem)
+                            items.append(contentsOf: hasAppDeviceItems)
+                            return items
+                        }
+                    }
+                    
+                    if self.runtimes.contains(where: { $0.devices.contains(where: { $0.applications.isEmpty }) }) {
+                        deviceItems.append(NSMenuItem.separator())
+                    }
+                    
+                    if deviceItems.isEmpty {
+                        let xcodeSelectItem = NSMenuItem(title: "Xcode Select...",
+                                                         action: #selector(self.openPreferences),
+                                                         keyEquivalent: "")
+                        xcodeSelectItem.target = self
+                        deviceItems.append(xcodeSelectItem)
+                    }
+                    
+                    deviceItems.append(NSMenuItem.separator())
+                    deviceItems.append(contentsOf: self.commonItems)
+                    
+                    DispatchQueue.main.async {
+                        self.removeAllItems()
+                        deviceItems.forEach { self.addItem($0) }
+                    }
+                case .failure(let error):
+                    error.displayAlert()
+                }
+            case .failure(let error):
+                error.displayAlert()
             }
         }
         self.refreshTask = task
@@ -202,5 +209,20 @@ final class MainMenu: NSMenu {
         let preferenceWindowController = storyboard.instantiateController(withIdentifier: "Preferences") as? NSWindowController
         NSApp.activate(ignoringOtherApps: true)
         preferenceWindowController?.window?.makeKeyAndOrderFront(NSApplication.shared)
+    }
+}
+
+extension XCRunError {
+    func displayAlert() {
+        DispatchQueue.main.async {
+            let alert: NSAlert = NSAlert()
+            alert.messageText = "Error running: \(command)"
+            alert.informativeText = message
+            alert.alertStyle = .critical
+            alert.addButton(withTitle: "OK")
+            NSApp.activate(ignoringOtherApps: true)
+            
+            alert.runModal()
+        }
     }
 }
